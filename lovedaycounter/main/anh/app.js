48c76c079a2b71be6dd414ed9943876bc12edb85
Drake Strike <phjtieudoc@users.noreply.github.com>
2020-06-26 19:16:59 +0700

    Update app.js

--- lovedaycounter/main/anh/app.js
+++ lovedaycounter/main/anh/app.js
@@ -1,5 +1,5 @@
 const yourDate = new Date("2020-05-05T23:11:00"),
-music = ['noinaycoanh'];
+music = ['noinaycoanh', 'ido'];
 
 document.addEventListener('DOMContentLoaded', function(){
       var rootTime = document.querySelector("time");
